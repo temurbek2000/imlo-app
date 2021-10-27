cc1e616f7f4a17ac4053b4ffe21429f403aef389
temurbek2000 <86454410+temurbek2000@users.noreply.github.com>
2021-10-27 23:19:09 +0500

    first

---
+++ src/commonComponents/footer/footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    Container
+} from "react-bootstrap";
+
+class FooterComponent extends React.Component{
+    render() {
+        return (
+            <div className="p-3" style={{width:'100%',minHeight:'150px',backgroundColor:'black'}}>
+              <Container>
+                  <div className="d-flex p-4">
+                      <p style={{color:'white',fontSize:'22px',fontWeight:'bold'}}>
+                          Copyright © 2021 . Imlo Manbai
+                         <div className="d-flex text-center">
+                             <p style={{color:'white',fontSize:'18px',fontWeight:600}}>
+                                 Designed by <a href="http://t.me/IBRAGIMOV_ULUGBEK_2">Ibragimova Shakhnoza</a> <br/>
+                                 Programmer <a href="http://t.me/temurbek1401">Temurbek Ismoilov</a>
+                             </p>
+                         </div>
+                      </p>
+
+                  </div>
+              </Container>
+            </div>
+        );
+    }
+}
+export default FooterComponent;
